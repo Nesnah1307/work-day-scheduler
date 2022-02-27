841a8c4de5e9705e793c3efb91f4fca5be5c8898
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-02-27 13:47:23 -0700

    added .each() for time loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,35 @@ $(document).ready(function () {
         // save in localStorage
         localStorage.setItem(time, text);
     })
-})    
+    function hourLoop() {
+        
+        var currentHour = moment().hour();
+
+        // looping hours
+        $(".time-block").each(function (timer) {
+            var timeSlot = parseInt($(this).attr("id").split("hour")[1]);
+            
+            // loop will change color of .time-block depending on the time
+            if (timeSlot < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (timeSlot === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+            // verified .each() is reading each timeslot
+            console.log( timer + $( this ).text() );
+        })
+    }
+    hourLoop();
+})
+
+ 
