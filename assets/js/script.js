be3757c990aa4ad3541326019545454aff31583b
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-02-27 13:20:32 -0700

    readied document to load JS and verified buttons are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+// prepare page to load JS
+$(document).ready(function () {
+    console.log("Ready To Go!")
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        
+        console.log("Button was clicked.")
+        // save in localStorage
+        localStorage.setItem(time, text);
+    })
+})    
